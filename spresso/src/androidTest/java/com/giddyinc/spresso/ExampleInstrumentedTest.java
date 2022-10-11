48bdab5c06aea8e5da4ec4c482eaad0475cd6a39
Ray <ray@boxed.com>
2022-10-11 12:10:40 -0400

    wrong module type before

--- spresso/src/androidTest/java/com/giddyinc/spresso/ExampleInstrumentedTest.java
+++ spresso/src/androidTest/java/com/giddyinc/spresso/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.giddyinc.spresso", appContext.getPackageName());
+        assertEquals("com.giddyinc.spresso.test", appContext.getPackageName());
     }
 }
